2a6534b0f672bbb9e44812012c1e6ec659b9714d
dorothee <viard.dorothee@gmail.com>
2019-07-23 17:10:35 +0200

    feat: client / add cities container

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiClientService } from './api-client.service';
-import  { City } from './models/City';
-import  { CurrentWeather } from './models/CurrentWeather';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  cities: City[];
-
-  constructor(private api: ApiClientService) { }
-
-  ngOnInit() {
-    this.getCities();
-  }
-
-  getCities(): void {
-    this.api.getCities()
-    .subscribe(data => {
-      this.cities = data;
-    });
-  }
-}
+export class AppComponent {}
