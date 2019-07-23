d468d9f3fb603d7d29dd88385ad82750181cb047
dorothee <viard.dorothee@gmail.com>
2019-07-23 11:30:30 +0200

    feat: client / components & services creation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CityListComponent } from './components/city-list/city-list.component';
+import { CityListItemComponent } from './components/city-list-item/city-list-item.component';
+import { CityDetailsComponent } from './components/city-details/city-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityListComponent,
+    CityListItemComponent,
+    CityDetailsComponent
   ],
   imports: [
     BrowserModule,
