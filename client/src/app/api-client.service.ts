a5a8431800c9f037438e44e32e29b5ba67f951a7
dorothee <viard.dorothee@gmail.com>
2019-07-23 15:49:12 +0200

    feat: currentWeather parse & getCities

--- client/src/app/api-client.service.ts
+++ client/src/app/api-client.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { City } from './models/City';
+import { CurrentWeather } from './models/CurrentWeather';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,30 @@ export class ApiClientService {
 
   serverBaseUrl: string = 'http://localhost:4000';
   weatherApiBaseUrl: string = 'https://api.openweathermap.org/data/2.5';
-  apiKey: string = '';
+  apiKey: string = '4f510f6aeb0a818ddd35fad7382c0c9c';
 
   constructor(private http: HttpClient) { }
 
+  // Server
   getCities(): Observable<City[]> {
     return this.http.get<City[]>(`${this.serverBaseUrl}/getAll`);
   }
+
+  getCity(id): Observable<City> {
+    return this.http.get<City>(`${this.serverBaseUrl}/getOne/${id}`);
+  }
+
+  saveWeather(id, forecast): Observable<City> {
+    return this.http.put<City>(`${this.serverBaseUrl}/getOne/${id}`, forecast);
+  }
+
+  // Openweathermap
+  getCurrentWeather(city): Observable<any> {
+    return this.http.get<any>(`${this.weatherApiBaseUrl}/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+      .pipe(map(current => CurrentWeather.parse(current)));
+  }
+/*
+  getForecastWeather(city): Observable<any> {
+
+  }*/
 }
--- client/src/app/models/CurrentWeather.ts
+++ client/src/app/models/CurrentWeather.ts
@@ -1,7 +1,17 @@
 export class CurrentWeather {
-  humidity: number;
   temp: number;
+  humidity: number;
   description: string;
   icon: string;
+
+  static parse(data) {
+    const current = {
+      temp: data.main.temp.toFixed(1),
+      humidiy: data.main.humidity,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon
+    }
+    return current;
+  }
 }
 
