6cd05485994233f0e7e0a48efa64d8b060ab4488
dorothee <viard.dorothee@gmail.com>
2019-07-24 14:49:21 +0200

    feat: client / update weather container

--- client/src/app/containers/weather/weather.component.ts
+++ client/src/app/containers/weather/weather.component.ts
@@ -10,21 +10,36 @@ import { City } from '../../models/City';
 export class WeatherComponent implements OnInit {
 
   @Input() cities: City[];
-  @Output() save: EventEmitter<City> = new EventEmitter<City>();
 
   constructor(private api: ApiClientService) { }
 
   ngOnInit() {
-    this.cities.forEach(city => {
-      this.api.getCurrentWeather(city.name)
-      .subscribe(data => {
-        city.current_weather = data;
+    this.getCities();
+  }
+  getCities(): void {
+    this.api.getCities()
+    .subscribe(data => {
+      this.cities = data;
+      this.cities.forEach(city => {
+        this.api.getCurrentWeather(city.name)
+        .subscribe(data => {
+          city.current_weather = data;
+          console.log(this.cities)
+        });
       });
     });
   }
 
   onSave(city: City) {
-    this.save.emit(city);
+    this.api.getForecastWeather(city.city_id)
+    .subscribe(forecast => {
+      this.api.saveWeather(city._id, {forecast})
+      .subscribe(newCity => {
+        this.getCities();
+      })
+
+    })
   }
+
 
 }
