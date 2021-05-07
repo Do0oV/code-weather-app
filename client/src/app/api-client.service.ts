d4a90d0911a545fcb87b30c4b2589dd46d8d28c9
Dorothee Viard <viard.dorothee@gmail.com>
2021-05-07 18:12:40 +0200

    Feature/update deps (#26)
    * update server deps
    
    * update to angular8.0
    
    Co-authored-by: Dorothee Viard <dviard@zinio.com>

--- client/src/app/api-client.service.ts
+++ client/src/app/api-client.service.ts
@@ -1,26 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { City } from './models/City';
-import { CurrentWeather } from './models/CurrentWeather';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { City } from "./models/City";
+import { CurrentWeather } from "./models/CurrentWeather";
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
+    "Content-Type": "application/json",
+  }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiClientService {
+  serverBaseUrl: string = "http://localhost:4000";
+  weatherApiBaseUrl: string = "https://api.openweathermap.org/data/2.5";
+  apiKey: string = "";
 
-  serverBaseUrl: string = 'http://localhost:4000';
-  weatherApiBaseUrl: string = 'https://api.openweathermap.org/data/2.5';
-  apiKey: string = '';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Server
   getCities(): Observable<City[]> {
@@ -32,16 +31,25 @@ export class ApiClientService {
   }
 
   saveWeather(id, forecast): Observable<City> {
-    return this.http.put<City>(`${this.serverBaseUrl}/save/${id}`, forecast, httpOptions);
+    return this.http.put<City>(
+      `${this.serverBaseUrl}/save/${id}`,
+      forecast,
+      httpOptions
+    );
   }
 
   // Openweathermap
   getCurrentWeather(city): Observable<any> {
-    return this.http.get<CurrentWeather>(`${this.weatherApiBaseUrl}/weather?q=${city}&units=metric&appid=${this.apiKey}`)
-      .pipe(map(current => CurrentWeather.parse(current)));
+    return this.http
+      .get<CurrentWeather>(
+        `${this.weatherApiBaseUrl}/weather?q=${city}&units=metric&appid=${this.apiKey}`
+      )
+      .pipe(map((current) => CurrentWeather.parse(current)));
   }
 
   getForecastWeather(cityId): Observable<any> {
-    return this.http.get<any>(`${this.weatherApiBaseUrl}/forecast?id=${cityId}&units=metric&appid=${this.apiKey}`);
+    return this.http.get<any>(
+      `${this.weatherApiBaseUrl}/forecast?id=${cityId}&units=metric&appid=${this.apiKey}`
+    );
   }
 }
