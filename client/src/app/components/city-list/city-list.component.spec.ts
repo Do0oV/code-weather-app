823a912b9b194c73df27d5820ced5d0401fcb5f3
Dorothee Viard <dviard@zinio.com>
2021-05-07 20:26:07 +0200

    update client to angular11

--- client/src/app/components/city-list/city-list.component.spec.ts
+++ client/src/app/components/city-list/city-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CityListComponent } from './city-list.component';
 
@@ -6,7 +6,7 @@ describe('CityListComponent', () => {
   let component: CityListComponent;
   let fixture: ComponentFixture<CityListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CityListComponent ]
     })
